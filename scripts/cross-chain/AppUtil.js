f3d2de57d97983f34c91d8b56af7785656cf930f
Maulana-anjari <maulana17anjari@gmail.com>
2024-01-23 12:13:41 +0700

    fix the org1 & org4 ccp path on the AppUtils.js to the real project

--- scripts/cross-chain/AppUtil.js
+++ scripts/cross-chain/AppUtil.js
@@ -11,7 +11,7 @@ const path = require('path');
 
 exports.buildCCPOrg1 = () => {
 	// load the common connection configuration file
-	const ccpPath = path.resolve(__dirname, '..', '..', 'fabric', 'bc-hospital', 'organizations', 'peerOrganizations', 'org1.hospital.com', 'connection-org1.json');
+	const ccpPath = path.resolve(__dirname, '..', '..', '..', 'shared', 'hospital', 'organizations', 'peerOrganizations', 'org1.hospital.com', 'connection-org1.json');
 	const fileExists = fs.existsSync(ccpPath);
 	if (!fileExists) {
 		throw new Error(`no such file or directory: ${ccpPath}`);
@@ -27,7 +27,7 @@ exports.buildCCPOrg1 = () => {
 
 exports.buildCCPOrg4 = () => {
 	// load the common connection configuration file
-	const ccpPath = path.resolve(__dirname, '..', '..', 'fabric', 'bc-insurance', 'organizations', 'peerOrganizations', 'org4.insurance.com', 'connection-org4.json');
+	const ccpPath = path.resolve(__dirname, '..', '..', '..', 'shared', 'insurance', 'organizations', 'peerOrganizations', 'org4.insurance.com', 'connection-org4.json');
 	const fileExists = fs.existsSync(ccpPath);
 	if (!fileExists) {
 		throw new Error(`no such file or directory: ${ccpPath}`);
