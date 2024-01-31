b6dcb84244899b0b0ca00e6717ab0c8ca86022f4
novaldypratama <novaldypratama@mail.ugm.ac.id>
2024-01-31 09:32:08 +0700

    feat: add ssh tunneling and some logging

--- scripts/orchestration/start-hlfnet.sh
+++ scripts/orchestration/start-hlfnet.sh
@@ -2,6 +2,8 @@
 
 # Function to log messages
 log() {
+    echo ""
+    echo ""
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
 }
 
@@ -19,8 +21,8 @@ fi
 sleep 5
 
 # Hostnames or IP addresses of the two remote hosts
-HOST1='novaldy@10.42.10.132'
-HOST2='gdputra@10.42.10.131'
+HOST1='novaldy@10.42.10.132' # guntur-labse     - bc-hospital
+HOST2='gdputra@10.42.10.131' # guntur-labse2    - bc-insurance
 
 # Command to execute on the remote hosts
 REMOTE_COMMAND_HOSPITAL='if [ -d ~/pharma-chain/fabric/bc-hospital ] && [ -x ~/pharma-chain/fabric/bc-hospital/network.sh ]; then export PATH=$PATH:$HOME/fabric-samples/bin && cd ~/pharma-chain/fabric/bc-hospital && ./network.sh up createChannel -c hospital -ca; else echo "Required files not found on HOST1"; fi'
@@ -29,9 +31,21 @@ REMOTE_COMMAND_INSURANCE='if [ -d ~/pharma-chain/fabric/bc-insurance ] && [ -x ~
 # SSH options
 SSH_OPTIONS='-o ConnectTimeout=10 -o StrictHostKeyChecking=no'
 
+log "Starting bc-hospital on guntur-labse ..."
 # Execute command on HOST1
 ssh $SSH_OPTIONS $HOST1 "$REMOTE_COMMAND_HOSPITAL"
 
+# wait for a few seconds
+sleep 2
+
+log "Starting bc-insurance on guntur-labse2 ..."
 # Execute command on HOST2
 ssh $SSH_OPTIONS $HOST2 "$REMOTE_COMMAND_INSURANCE"
 
+# SSH tunnelling for bc-hospital (guntur-labse)
+log "Starting ssh port tunnelling for bc-hospital..."
+ssh -L 7050:localhost:7050 -L 7021:localhost:7021 -L 7031:localhost:7031 -N -f $HOST1
+
+# SSH tunnelling for bc-insurance (guntur-labse2)
+log "Starting ssh port tunnelling for bc-insurance..."
+ssh -L 7150:localhost:7150 -L 7021:localhost:7121 -L 7031:localhost:7131 -N -f $HOST2
